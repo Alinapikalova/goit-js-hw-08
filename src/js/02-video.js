9e95c2674799e917cea8f7254f8962790a72d0de
Alinapikalova <alina.pikalova@gmail.com>
2023-06-07 14:31:14 +0200

    update 2 home work for vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,13 +10,23 @@ player.getVideoTitle().then(function (title) {
 });
 
 //on
+
 player.on('eventName', function (data) {
   // data is an object containing properties specific to that event
 });
 const onPlay = function (data) {
   // data is an object containing properties specific to that event
 };
+//addeventlistener
 
 player.on('play', onPlay);
+
+
+//currentTime
+{
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+}
 
 

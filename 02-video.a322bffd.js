c4f9cce610ea32629140d6765a6168bd789051ef
Alinapikalova <122531434+Alinapikalova@users.noreply.github.com>
2023-06-07 13:03:42 +0000

    Deploying to gh-pages from @ Alinapikalova/goit-js-hw-08@3f5ff201fdce1a8c3a6315b81aea4cab1c13b422 🚀

--- 02-video.a322bffd.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.on("eventName",(function(e){}));o.on("play",(function(e){}));
-//# sourceMappingURL=02-video.a322bffd.js.map
--- 02-video.e9607b36.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("play",(function(){console.log("played the video!")})),n.getVideoTitle().then((function(e){console.log("title:",e)})),n.on("eventName",(function(e){}));n.on("play",(function(e){}))}();
-//# sourceMappingURL=02-video.e9607b36.js.map

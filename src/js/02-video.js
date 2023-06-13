283cbaac8dfcb81c6079f623f5375efd9c7c966f
Alinapikalova <alina.pikalova@gmail.com>
2023-06-13 12:49:45 +0200

    add setcurenttime and throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,22 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-// var throttle = require('lodash.throttle');
+var throttle = require('lodash.throttle');
 
 //on
 
 const onPlay = function (data) {
   console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
   // data is an object containing properties specific to that event
 };
-//addeventlistener
+//addeventlistene
 
 console.log(player);
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let time = localStorage.getItem('videoplayer-current-time');
+if (time !== undefined) {
+  player.setCurrentTime(Number(time));
+}
